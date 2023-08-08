c0df82dbc0f43ca6cedecb0f5aa293166d57af8c
bansalgautam <gautamdevelops@gmail.com>
2023-08-08 22:36:40 +0530

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-function App() {
+import { useState } from "react";
+import "./App.css";
 
+function App() {
+  const [active, setActive] = useState("hidden");
   return (
-    <>
-      <h1>Hello, World</h1>
-    </>
-  )
+    <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+      <button
+        onClick={() => {
+          setActive("block");
+        }}
+        className="bg-gray-900 text-white px-2 py-1 text-2xl"
+      >
+        Click Here Gandu
+      </button>
+      <h1 className={`${active} text-5xl`}>Bhag Bosdike</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
